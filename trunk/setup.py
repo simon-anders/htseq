9463364f41a4b19feb33e8da7ccd53c2fdf61cef
sanders_muc <sanders_muc@b9189600-30aa-42b7-bd87-37424b8c7c3a>
2010-01-20 16:42:10 +0000

    made proper distutil dir structure

---
+++ trunk/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+import os.path
+try:
+   import numpy
+except ImportError:
+   sys.stderr.write( "Setup script for HTSeq: Failed to import 'numpy'.\n" )
+   sys.stderr.write( "Please install numpy and then try again to install HTSeq.\n" )
+   
+numpy_include_dir = os.path.join( os.path.dirname( numpy.__file__ ), 'core', 'include' )
+ 
+
+setup( name = 'HTSeq',
+       version = '0.3',
+       description = 'Processing high-throughput sequencing data',
+       author = 'Simon Anders',
+       author_email = 'sanders@fs.tum.de',
+       url = 'http://htseq.sf.net',
+       
+       py_modules = [ 
+          'HTSeq._HTSeq_internal', 
+          'HTSeq.StepVector' 
+       ],
+       ext_modules = [ 
+          Extension( 'HTSeq._HTSeq', 
+             ['src/_HTSeq.c'], include_dirs=[numpy_include_dir], extra_compile_args=['-w'] ),
+          Extension( 'HTSeq._StepVector', 
+             ['src/StepVector_wrap.cxx'], extra_objects=['src/step_vector.h'], extra_compile_args=['-w'] ),
+       ]
+     )
+
+
