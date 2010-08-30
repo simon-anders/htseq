751686d15a0b9dd52756fc4e511a273ff8a0bd22
quetzcoatl <PaulPyl@users.noreply.github.com>
2010-08-30 11:24:56 +0000

    More dirt and golfballs . . . maybe we should hope for bacteria that dwell on leaked memory to clean up after this mess :\

--- test/dsv_cpp_test.cpp
+++ test/dsv_cpp_test.cpp
@@ -6,8 +6,14 @@ int main()
 {
     DSV< int, std::string > dsv;
     dsv.set( 3, 6, std::string( "Foo" ) );
-    //dsv.set( 10, 15, std::string( "Bar" ) );
+    dsv.set( 10, 15, std::string( "Bar" ) );
+    dsv.set( 12, 13, std::string( "Wakka" ) );
+    dsv.set( 12, 13, std::string( "Quacko" ) );
+    for( int i = 0; i < 20; i++ )
+       std::cout << i << ": " << dsv.get(i) << std::endl;
+    std::cout << "Overwriting with Zeroes!" << std::endl;
     dsv.set( 0, 100, std::string( "000" ) );
     for( int i = 0; i < 20; i++ )
        std::cout << i << ": " << dsv.get(i) << std::endl;
+    dsv.clear();
 }
