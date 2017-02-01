73adfbdc9a30c2dafa24472e6837807a953450ee
yhoogstrate <y.hoogstrate@gmail.com>
2017-02-01 15:16:21 +0100

    pick swig from conda

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -2,13 +2,16 @@
 
 import unittest
 
+
 class TestFusion(unittest.TestCase):
     def test_01(self):
         import HTSeq
+        self.assertTrue(len(str(HTSeq.__version__)) > 0)
 
 
 def main():
     unittest.main()
 
+
 if __name__ == '__main__':
-	main()
+    main()
