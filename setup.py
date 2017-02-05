ed122bc4b1b030816ec293f0c87168f9b827bdf0
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-02-05 15:07:10 -0800

    Improvs in README, build script, setup.py, and new maintainer mention

--- setup.py
+++ setup.py
@@ -4,66 +4,72 @@ import sys
 import os.path
 
 try:
-   from setuptools import setup, Extension
-except ImportError:   
-   sys.stderr.write( "Could not import 'setuptools', falling back to 'distutils'.\n" )
-   from distutils.core import setup, Extension
+    from setuptools import setup, Extension
+except ImportError:
+    sys.stderr.write("Could not import 'setuptools', falling back to 'distutils'.\n")
+    from distutils.core import setup, Extension
 
-if sys.version_info[0] < 3:
-   sys.stderr.write( "Error in setup script for HTSeq:\n" )
-   sys.stderr.write( "Sorry, this version of HTSeq is for Python 3 only.\n" )
-   sys.exit( 1 )
+if (sys.version_info[0] < 3) or ((sys.version_info[0] == 3) and (sys.version_info[1] < 4)):
+    sys.stderr.write("Error in setup script for HTSeq:\n")
+    sys.stderr.write("Sorry, this version of HTSeq is for Python 3.4 or higher.\n")
+    sys.exit(1)
 
 try:
-   import numpy
+    import numpy
 except ImportError:
-   sys.stderr.write( "Setup script for HTSeq: Failed to import 'numpy'.\n" )
-   sys.stderr.write( "Please install numpy and then try again to install HTSeq.\n" )
-   sys.exit( 1 )
-   
-numpy_include_dir = os.path.join( os.path.dirname( numpy.__file__ ), 'core', 'include' )
+    sys.stderr.write("Setup script for HTSeq: Failed to import 'numpy'.\n")
+    sys.stderr.write("Please install numpy and then try again to install HTSeq.\n")
+    sys.exit(1)
+
+numpy_include_dir = os.path.join(os.path.dirname(numpy.__file__), 'core', 'include')
 
 
-def get_version():
-    with open("VERSION","r") as fh:
-        return fh.read().strip().strip('%')
+# Update version from VERSION file into module
+with open('VERSION') as fversion:
+    version = fversion.readline().rstrip()
+with open('HTSeq/_version.py', 'wt') as fversion:
+    fversion.write('__version__ = "'+version+'"')
 
 
-setup( name = 'HTSeq',
-       version = get_version(),
-       author = 'Simon Anders',
-       author_email = 'sanders@fs.tum.de',
-       url = 'http://www-huber.embl.de/users/anders/HTSeq/',
-       description = "A framework to process and analyze data from " +
-          "high-throughput sequencing (HTS) assays",
-       classifiers = [
-          'Development Status :: 5 - Production/Stable',
-          'Topic :: Scientific/Engineering :: Bio-Informatics',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Operating System :: POSIX',
-          'Programming Language :: Python'
-       ],
-       requires = [ 'numpy', 'python (>=3.0)' ],
-       
-       py_modules = [ 
-          'HTSeq._HTSeq_internal', 
-          'HTSeq.StepVector',
-          'HTSeq._version',
-          'HTSeq.scripts.qa',
-          'HTSeq.scripts.count'
-       ],
-       ext_modules = [ 
-          Extension( 'HTSeq._HTSeq', 
-             ['src/_HTSeq.c'], include_dirs=[numpy_include_dir], extra_compile_args=['-w'] ),
-          Extension( 'HTSeq._StepVector', 
-             ['src/StepVector_wrap.cxx'], extra_compile_args=['-w'] ),
-       ],
-       scripts = [
-          'scripts/htseq-qa',
-          'scripts/htseq-count',
-       ]
-     )
-
-
+setup(name='HTSeq',
+      version=version,
+      author='Simon Anders',
+      author_email='sanders@fs.tum.de',
+      maintainer='Fabio Zanini',
+      maintainer_email='fabio.zanini@stanford.edu',
+      url='http://www-huber.embl.de/users/anders/HTSeq/',
+      description="A framework to process and analyze data from " +
+                  "high-throughput sequencing (HTS) assays",
+      classifiers=[
+         'Development Status :: 5 - Production/Stable',
+         'Topic :: Scientific/Engineering :: Bio-Informatics',
+         'Intended Audience :: Developers',
+         'Intended Audience :: Science/Research',
+         'License :: OSI Approved :: GNU General Public License (GPL)',
+         'Operating System :: POSIX',
+         'Programming Language :: Python'
+      ],
+      requires=['numpy', 'python (>=3.4)'],
+      py_modules=[
+         'HTSeq._HTSeq_internal',
+         'HTSeq.StepVector',
+         'HTSeq._version',
+         'HTSeq.scripts.qa',
+         'HTSeq.scripts.count'
+      ],
+      ext_modules=[
+         Extension(
+             'HTSeq._HTSeq',
+             ['src/_HTSeq.c'],
+             include_dirs=[numpy_include_dir],
+             extra_compile_args=['-w']),
+         Extension(
+             'HTSeq._StepVector',
+             ['src/StepVector_wrap.cxx'],
+             extra_compile_args=['-w']),
+      ],
+      scripts=[
+         'scripts/htseq-qa',
+         'scripts/htseq-count',
+      ]
+      )
