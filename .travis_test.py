a9f3975691bb5979cd6960be554ef521a638546d
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-03-21 14:05:37 -0700

    Fix print in test script

--- .travis_test.py
+++ .travis_test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import sys
 import subprocess
 
-#subprocess.call('ls', shell=True)
 py_fdn = 'python'+str(sys.version_info[0])+'/'
 print('py_fdn:', py_fdn)
+print('Running tests...')
 subprocess.call('python '+py_fdn+'test/test.py', shell=True)
+print('done!')
