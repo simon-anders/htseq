2b441800efda5f2c6ff72210063fc42fca53971c
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-03-21 09:55:15 -0700

    no ls but print py_fdn

--- .travis_test.py
+++ .travis_test.py
@@ -2,6 +2,7 @@
 import sys
 import subprocess
 
-subprocess.call('ls', shell=True)
+#subprocess.call('ls', shell=True)
 py_fdn = 'python'+str(sys.version_info[0])+'/'
+print('py_fdn:', py_fdn)
 subprocess.call('python '+py_fdn+'test/test.py', shell=True)
