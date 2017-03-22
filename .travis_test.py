31ba12325354ca25e033d91c3315265c6356a1be
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-03-22 13:34:09 -0700

    Travis test script (for Py2/3 compatibility)

---
+++ .travis_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import subprocess
+
+py_fdn = 'python'+str(sys.version_info[0])+'/'
+print('py_fdn:', py_fdn)
+print('Running tests...')
+subprocess.call('python '+py_fdn+'test/test.py', shell=True)
+print('done!')
