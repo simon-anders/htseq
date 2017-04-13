81b2fd093a56161fe7d91702942f48d283b3e5cd
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-04-13 11:06:29 -0700

    Travis test fixed?

--- .travis_test.py
+++ .travis_test.py
@@ -6,5 +6,7 @@ import subprocess
 py_fdn = 'python'+str(sys.version_info[0])+'/'
 print('py_fdn:', py_fdn)
 print('Running tests...')
-subprocess.call('python '+py_fdn+'test/test.py', shell=True)
+retcode = subprocess.call('python '+py_fdn+'test/test.py', shell=True)
+if retcode:
+    sys.exit(retcode)
 print('done!')
