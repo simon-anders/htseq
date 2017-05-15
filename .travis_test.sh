709e07e9464973edf6eb710fa453c7421a505853
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-05-14 21:09:17 -0700

    Test should work now

--- .travis_test.sh
+++ .travis_test.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-py_fdn=python$(python --version | cut -f2 -d' ' | cut -f1 -d'.')
-echo "py_fdn: ${py_fdn}/"
+py_fdn="python$(python -c 'from __future__ import print_function; import sys; print(sys.version_info[0])')"
+echo "py_fdn: ${py_fdn}"
 
 echo 'Running tests...'
 
 echo 'Doctests...'
 python "${py_fdn}test/test.py"
+if [ $? != 0 ]; then
+    exit 1
+fi
 echo 'done!'
 
 echo 'htseq-count...'
 python "${py_fdn}test/test_htseq-count.py"
+if [ $? != 0 ]; then
+    exit 1
+fi
 echo 'done!'
