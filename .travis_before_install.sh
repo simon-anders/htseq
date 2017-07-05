2bedb48007570c331f8c7154911172a0750b5ffb
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-07-05 16:27:57 -0700

    Try to include OSX

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-sudo add-apt-repository ppa:nschloe/swig-backports -y
-sudo apt-get -qq update
-sudo apt-get install -y swig3.0
+if [ $TRAVIS_OS_NAME == 'linux' ]; then
+  echo "Installing deps for linux"
+  sudo add-apt-repository ppa:nschloe/swig-backports -y
+  sudo apt-get -qq update
+  sudo apt-get install -y swig3.0
+elif [ $TRAVIS_OS_NAME == 'osx' ]; then
+  echo "Installing deps for OSX"
+  #TODO
+else
+ echo "OS not recognized: $TRAVIS_OS_NAME"
+ exit 1
+fi
