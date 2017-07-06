bd9cac41e1b428b1fb053e1b8a0a1e71ad7a0cc7
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-07-05 18:05:43 -0700

    Generic language for OSX

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -7,6 +7,18 @@ if [ $TRAVIS_OS_NAME == 'linux' ]; then
 elif [ $TRAVIS_OS_NAME == 'osx' ]; then
   echo "Installing deps for OSX"
   #TODO
+  if [ $PYTHON_VERSION == "2.7" ]; then
+    CONDA_VER='2'
+  elif [ $PYTHON_VERSION == "3.6" ]; then
+    CONDA_VER='3'
+  else
+    echo "Conda only supports 2.7 and 3.6"
+  fi
+  curl "https://repo.continuum.io/miniconda/Miniconda${CONDA_VER}-latest-MacOSX-x86_64.sh" -o "miniconda.sh"
+  bash "miniconda.sh" -b -p $HOME/miniconda
+  export PATH="$HOME/miniconda/bin:$PATH"
+  source $HOME/miniconda/bin/activate
+
 else
  echo "OS not recognized: $TRAVIS_OS_NAME"
  exit 1
