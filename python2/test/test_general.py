fd15cbc406fa2ef6ffe67dc118861d022ba455bb
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-07-13 10:20:52 -0700

    Tests from master branch

---
+++ python2/test/test_general.py
@@ -0,0 +1,86 @@
+from __future__ import print_function
+import sys
+import os
+import glob
+import distutils.util
+import doctest
+
+build_dir = "build/lib.%s-%s" % (distutils.util.get_platform(), sys.version[0:3])
+
+sys.path.insert(0, os.path.join(os.getcwd(), build_dir))
+import HTSeq
+
+py_fdn = 'python'+str(sys.version_info[0])
+
+
+def test_fasta_parser():
+    print("Test Fasta parser")
+    for seq in HTSeq.FastaReader('example_data/fastaExLong.fa'):
+        pass
+    print("Test passed")
+    print("Test Fasta parser (raw iterator)")
+    for seq in HTSeq.FastaReader('example_data/fastaExLong.fa',
+                                 raw_iterator=True):
+        pass
+    print("Test passed")
+
+
+def test_fastq_parser():
+    print("Test Fastq parser")
+    for seq in HTSeq.FastqReader('example_data/fastqEx.fastq'):
+        pass
+    print("Test passed")
+    print("Test Fastq parser on gzip input")
+    for seq in HTSeq.FastqReader('example_data/fastqExgzip.fastq.gz'):
+        pass
+    print("Test passed")
+    print("Test Fastq parser on gzip input (raw iterator)")
+    for seq in HTSeq.FastqReader('example_data/fastqExgzip.fastq.gz',
+                                 raw_iterator=True):
+        pass
+    print("Test passed")
+
+
+def test_bam_inconsistent_mate():
+    print('Test inconsistent BAM file')
+    bamfile = HTSeq.BAM_Reader("example_data/inconsistent_mate.bam")
+    for read in bamfile:
+        pass
+    print("Test passed")
+
+
+def test_pickle():
+    import pickle
+
+    print('Test pickling and inpickling')
+    pickles = [
+            {'name': 'HTSeq.Sequence',
+             'object': HTSeq.Sequence(b'ACTG', 'sequence'),
+             'assert_properties': ('seq', 'name', 'descr')},
+            ]
+
+    for pic in pickles:
+        print('Pickling '+pic['name'])
+        pickled = pickle.dumps(pic['object'])
+        print('Done')
+
+        print('Unpickling '+pic['name'])
+        unpick = pickle.loads(pickled)
+        print('Done')
+
+        if 'assert_properties' in pic:
+            print('Checking serialized/deserialized')
+            for prop in pic['assert_properties']:
+                assert getattr(pic['object'], prop) == getattr(unpick, prop)
+            print('Done')
+    print("Test passed")
+
+
+if len(sys.argv) == 1:
+    test_fasta_parser()
+    test_fastq_parser()
+    test_bam_inconsistent_mate()
+    test_pickle()
+else:
+    print("Wrong usage")
+    print("Call without arguments to run all tests")
---
+++ python2/test/test_htseq-count.py
@@ -0,0 +1,64 @@
+import subprocess as sp
+
+tests = [
+    {'call': [
+        'htseq-count',
+        '-m', 'intersection-nonempty',
+        '--nonunique', 'none',
+        'example_data/yeast_RNASeq_excerpt_withNH.sam',
+        'example_data/Saccharomyces_cerevisiae.SGD1.01.56.gtf.gz',
+        ],
+    'expected_fn': 'example_data/yeast_RNASeq_excerpt_withNH_counts.tsv'},
+    {'call': [
+        'htseq-count',
+        '-m', 'intersection-nonempty',
+        '--nonunique', 'all',
+        'example_data/yeast_RNASeq_excerpt_withNH.sam',
+        'example_data/Saccharomyces_cerevisiae.SGD1.01.56.gtf.gz',
+        ],
+    'expected_fn': 'example_data/yeast_RNASeq_excerpt_withNH_counts_nonunique.tsv'},
+    {'call': [
+        'htseq-count',
+        '-m', 'intersection-nonempty',
+        '-i', 'gene_id',
+        '--additional-attr', 'gene_name',
+        '--nonunique', 'none',
+        'example_data/yeast_RNASeq_excerpt_withNH.sam',
+        'example_data/yeast_RNASeq_excerpt_withNH.sam',
+        'example_data/Saccharomyces_cerevisiae.SGD1.01.56.gtf.gz',
+        ],
+    'expected_fn': 'example_data/yeast_RNASeq_excerpt_withNH_counts_twocolumns.tsv'},
+    {'call': [
+        'htseq-count',
+        '-m', 'intersection-nonempty',
+        '--nonunique', 'none',
+        '--secondary-alignments', 'ignore',
+        'example_data/yeast_RNASeq_excerpt_withNH.sam',
+        'example_data/Saccharomyces_cerevisiae.SGD1.01.56.gtf.gz',
+        ],
+    'expected_fn': 'example_data/yeast_RNASeq_excerpt_withNH_counts_ignore_secondary.tsv'},
+    ]
+
+
+# Run the tests
+for t in tests:
+    expected_fn = t['expected_fn']
+    call = t['call']
+    # local testing
+    #call = ['python2.7', 'python2/HTSeq/scripts/count.py'] + call[1:]
+
+    print(' '.join(call))
+    output = sp.check_output(call)
+
+    with open(expected_fn, 'r') as f:
+        expected = f.read()
+
+    try:
+        assert output == expected
+    except AssertionError:
+        for out, exp in zip(output.split('\n'), expected.split('\n')):
+            print(out, exp)
+            if out != exp:
+                break
+
+        raise
